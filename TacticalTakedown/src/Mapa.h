61be5a4ad6f4175d4ceb4143d712ddbb0d76f96f
maasensio <47762125+maasensio@users.noreply.github.com>
2021-05-09 21:16:05 +0200

    Revert "Mapa actualizado"
    This reverts commit e7f0847e28a4ad049a0d388f880726ed0c2a4b93.

--- TacticalTakedown/src/Mapa.h
+++ TacticalTakedown/src/Mapa.h
@@ -1,17 +1,15 @@
 #pragma once
-#include "Pared.h"
+//#include "Pared.h"
 
 
 class Mapa {
-private:
-	Pared borde_arriba, borde_abajo, borde_izq, borde_dcha;
+//private:
+	//Pared techo, suelo, pared_dcha, pared_izda;
 public:
 	Mapa();
 	//virtual ~Mapa();
 	void dibuja();
 	void copia_nivel1();
-	void textura(int fil, int col, float altura, char const* cadena1);
 
 	friend class Interaccion;
-	void textura();
 };
